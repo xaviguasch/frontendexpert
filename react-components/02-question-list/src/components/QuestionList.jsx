5ebd193d607a4f0971df4a33be43b3e9e456aa9a
Xavi Guasch <xaviguasch@gmail.com>
2023-05-29 11:12:05 +0200

    organized json data correctly for react challenge 02-question-list

--- react-components/02-question-list/src/components/QuestionList.jsx
+++ react-components/02-question-list/src/components/QuestionList.jsx
@@ -3,9 +3,39 @@ import React, { useState, useEffect } from 'react'
 import { dataQuestions } from '../data/question'
 import { dataSubmissions } from '../data/submissions'
 
+const getQuestionsByCategory = (qs) => {
+  const qsByCat = {}
+
+  qs.forEach(({ category, ...question }) => {
+    if (!qsByCat[category]) {
+      qsByCat[category] = []
+    }
+
+    qsByCat[category].push(question)
+  })
+
+  return qsByCat
+}
+
+const getSubmissionsByQ = (submissions) => {
+  const submissionByQuestion = {}
+
+  submissions.forEach(({ status, questionId }) => {
+    return (submissionByQuestion[questionId] = status)
+  })
+
+  return submissionByQuestion
+}
+
 const QuestionList = () => {
   const [questions, setQuestions] = useState(dataQuestions)
   const [submissions, setSubmissions] = useState(dataSubmissions)
+
+  const questionsByCategory = getQuestionsByCategory(questions)
+  const submissionsByQ = getSubmissionsByQ(submissions)
+
+  console.log(questionsByCategory)
+  console.log(submissionsByQ)
 
   return <div>QuestionList</div>
 }
